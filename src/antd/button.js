b533e42abd43a4f8b89c6c6f1f0ab142c57ea2b6
fst <1172088356@qq.com>
2019-05-06 15:55:06 +0800

    Add antd

---
+++ src/antd/button.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import {Button} from 'antd';
+class AntdButton extends Component {
+  render() {
+    return (
+        <Button></Button>
+    );
+  }
+}
+
+export default AntdButton;
